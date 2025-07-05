447a186c67601f7cce7146a96333640daa91684b
AqibNiazi <aqibjaved5201@gmail.com>
2025-07-05 17:15:30 +0500

    Remove to cart handler added

--- src/components/Shop/Products.jsx
+++ src/components/Shop/Products.jsx
@@ -1,16 +1,32 @@
 import ProductItem from './ProductItem';
 import classes from './Products.module.css';
-
+const DummyProducts = [
+  {
+    id: "p1",
+    price: 6,
+    title: "First Product",
+    description: "This is a first product - amazing!",
+  },
+  {
+    id: "p2",
+    price: 5,
+    title: "Second Product",
+    description: "This is a second product - amazing!",
+  },
+];
 const Products = (props) => {
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
+        {DummyProducts.map((product) => (
+          <ProductItem
+            key={product.id}
+            title={product.title}
+            price={product.price}
+            description={product.description}
+          />
+        ))}
       </ul>
     </section>
   );
--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 import uiReducer from "./UI-Slice";
-
+import cartRuducer from "./Cart-Slice";
 const store = configureStore({
   reducer: {
     ui: uiReducer,
+    cart: cartRuducer,
   },
 });
 export default store;
